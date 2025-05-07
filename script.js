2b52c817967205163d1158303b05b6586cdd890c
mheumann <184988905+mheumann37@users.noreply.github.com>
2025-05-07 08:10:27 -0500

    Write logic to get the computer choice
    Created a function that randomly returns 1 of 3 choices: rock, paper,
    or scissors. Called the function and stored the result in a variable.

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3) + 1;
+    //console.log(choice);
+    switch (choice) {
+        case 1:
+            return "rock";
+            break;
+        case 2:
+            return "paper";
+            break;
+        case 3:
+            return "scissors";
+            break;
+    }
+}
+let computerChoice = getComputerChoice();
+//console.log(computerChoice);
+
